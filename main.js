d27ad5db83daeadde1edd5a98bbc1b6e676113ef
epicsven-cup <jackylin616@gmail.com>
2025-09-12 20:19:46 -0500

    clearn up

--- main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ollamaClient_1 = require("./src/clients/ollamaClient");
-const client = new ollamaClient_1.OllamaClient();
-console.log("Hello world");
-console.log(client.listModel());
-//# sourceMappingURL=main.js.map
--- src/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ollamaClient_1 = require("./clients/ollamaClient");
-var client = new ollamaClient_1.OllamaClient();
-console.log("Hello world");
-console.log(client.listModel());
